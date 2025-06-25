8ab6aaf6c714fa00988d5a4a7a5910a49efa1f9a
IC Tech <148203290+ivancroce@users.noreply.github.com>
2025-06-25 15:43:44 +0100

    implements Customers, Products, Orders in Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,66 @@
+import com.github.javafaker.Faker;
+import entities.Customer;
+import entities.Order;
+import entities.Product;
+import enums.OrderStatus;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
+        Faker faker = new Faker(Locale.ITALY);
+
+        // --------------- Customers -------------
+
+        Customer customer1 = new Customer(001L, faker.dragonBall().character(), 2);
+        Customer customer2 = new Customer(002L, faker.dragonBall().character(), 3);
+        Customer customer3 = new Customer(003L, faker.dragonBall().character(), 1);
+        Customer customer4 = new Customer(004L, faker.dragonBall().character(), 3);
+        Customer customer5 = new Customer(005L, faker.dragonBall().character(), 1);
+
+        System.out.println(" --- Customers --- ");
+        // allCustomers List
+        List<Customer> allCustomers = List.of(customer1, customer2, customer3, customer4, customer5);
+
+        // classic foreach
+        /* for (Customer c : allCustomers) {
+            System.out.println(c);
+        }*/
+
+        // functional foreach
+        allCustomers.forEach(c -> System.out.println(c));
+
+        // --------------- Products -------------
+
+        Product book1 = new Product(101L, faker.book().title(), "Books", 110.0);
+        Product book2 = new Product(102L, faker.book().title(), "Books", 90.0);
+        Product book3 = new Product(201L, faker.book().title(), "Baby", 25.5);
+        Product book4 = new Product(301L, faker.book().title(), "Boys", 19.9);
+        Product book5 = new Product(302L, faker.book().title(), "Boys", 29.9);
+
+        System.out.println(" --- Products --- ");
+
+        List<Product> allProducts = List.of(book1, book2, book3, book4, book5);
+        allProducts.forEach(b -> System.out.println(b));
+
+
+        // --------------- Orders -------------
+
+        Order order1 = new Order(1001L, OrderStatus.COMPLETED, LocalDate.of(2021, 3, 5), LocalDate.of(2021, 3, 10), List.of(book1), customer1);
+        Order order2 = new Order(1002L, OrderStatus.CANCELLED, LocalDate.of(2021, 3, 5), LocalDate.of(2021, 3, 10), List.of(book2), customer2);
+        Order order3 = new Order(1003L, OrderStatus.SHIPPED, LocalDate.of(2021, 5, 1), LocalDate.of(2021, 5, 5), List.of(book3), customer3);
+        Order order4 = new Order(1004L, OrderStatus.PENDING, LocalDate.of(2021, 3, 15), LocalDate.of(2021, 3, 20), List.of(book1), customer4);
+
+        System.out.println(" --- Orders --- ");
+
+        List<Order> allOrders = List.of(order1, order2, order3, order4);
+        allOrders.forEach(o -> System.out.println(o));
+
+        // --------------- Excersice 1 Books > 100 -------------
+        
 
     }
+
 }
--- src/entities/Order.java
+++ src/entities/Order.java
@@ -1,4 +1,61 @@
 package entities;
 
+import enums.OrderStatus;
+
+import java.time.LocalDate;
+import java.util.List;
+
 public class Order {
+    private long id;
+    private OrderStatus status;
+    private LocalDate orderDate;
+    private LocalDate deliveryDate;
+    private List<Product> products;
+    private Customer customer;
+
+    public Order(Long id, OrderStatus status, LocalDate orderDate, LocalDate deliveryDate, List<Product> products, Customer customer) {
+        this.id = id;
+        this.status = status;
+        this.orderDate = orderDate;
+        this.deliveryDate = deliveryDate;
+        this.products = products;
+        this.customer = customer;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public LocalDate getOrderDate() {
+        return orderDate;
+    }
+
+    public LocalDate getDeliveryDate() {
+        return deliveryDate;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", status=" + status +
+                ", orderDate=" + orderDate +
+                ", deliveryDate=" + deliveryDate +
+                ", products=" + products +
+                ", customer=" + customer +
+                '}';
+    }
 }
+
